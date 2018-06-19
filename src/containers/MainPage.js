1106dda1a5e8b2ba5d16454f6443f6ea54aaa3a7
nevena29 <nevena.29@gmail.com>
2018-06-19 18:50:46 +0200

    post is not working

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -8,12 +8,23 @@ class MainPage extends Component{
     constructor(props){
         super(props);
         this.state = {
-             people : []
+             people : [],
+             postContact:{
+                 firstName: "",
+                 lastName:"",
+                 phone: ""
+
+             }
+
     }
 }
 componentDidMount(){
     this.getPeople();
 }
+componentWillMount(){
+    this.handleAddContact();
+}
+
 getPeople = (people) =>{
     fetch("http://localhost:4000/")
     .then(response=>response.json())
@@ -22,18 +33,19 @@ getPeople = (people) =>{
     
     
 }
+handleAddContact = (postContact) =>{
+    fetch("http://localhost:4000/add_contact", {
+        method: 'POST', 
+        body: JSON.stringify(postContact)
+        
+        })
+        .then(res => this.setState({postContact:postContact}))
+        .catch(error => console.error('Error:', error))
+        .then(response =>  console.log('Success:', response))
+}
 
 
-// handleAddContact = () => {
-//     this.setState(() => {
-       
-//     });
-// }
-// renderPeople =(people) =>{
-//     this.setState ({
-//         people:people
-//     })
-// }
+
 
     
     render(){
@@ -43,8 +55,7 @@ getPeople = (people) =>{
             <div>
             <Header/>
             <ListOfContacts people = {this.state.people} getPeople ={this.getPeople}/>
-            <FormContainer />
-            {/* handleAddContact = {this.handleAddContact}/> */}
+            <FormContainer handleAddContact = {this.handleAddContact}/>
             </div>
         )
     }
