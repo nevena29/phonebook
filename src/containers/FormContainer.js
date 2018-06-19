a899d80d2a50d02b3ef8cbca5be9879437c8f44f
nevena29 <nevena.29@gmail.com>
2018-06-20 00:46:58 +0200

    post request handled

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,18 +1,40 @@
 import React,{Component} from "react";
-import Form from "../components/Form";
+
 
 class FormContainer extends Component {
-        handleSubmits = (e) => {
+      constructor(props){
+          super(props);
+
+          this.state = {
+              formObject: {
+                  firstName: "",
+                  lastName: "",
+                  telephoneNumber: ""
+              }
+          }
+      }
+        handleSubmit = (e) => {
+            // console.log(e.target.name)
         e.preventDefault();
-        const firstName = e.target.firstName.value;
-        const lastName = e.target.lastName.value;
-        const telephoneNumber = e.target.telephoneNumber.value;
-        this.props.handleAddContact();
+        this.setState({
+            formObject:{
+                ...this.state.formObject,[e.target.name]: e.target.value
+            }
+        })
+       
     }
 
     render() {
         return (
-            <Form handleSubmits={this.handleSubmit} />
+            <div>
+            <form >
+                <input type="text" onChange={this.handleSubmit} placeholder="First Name" name="firstName" autoComplete="off" required value = {this.state.formObject.firstName}/>
+                <input type="text" onChange={this.handleSubmit} placeholder="Last Name" name="lastName" autoComplete="off" required value = {this.state.formObject.lastName}/>
+                <input type="text" onChange={this.handleSubmit} placeholder="Telephone Number" name="telephoneNumber" autoComplete="off" required value = {this.state.formObject.telephoneNumber}/>
+            </form>
+                <input type="submit" onClick={() => this.props.handleAddContact(this.state.formObject)} value="Add Contact"></input>
+            
+        </div>
         );
     }
 }
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -7,23 +7,22 @@ import FormContainer from "./FormContainer";
 class MainPage extends Component{
     constructor(props){
         super(props);
+        
         this.state = {
              people : [],
              postContact:{
                  firstName: "",
                  lastName:"",
                  phone: ""
-
              }
-
     }
 }
-componentDidMount(){
-    this.getPeople();
-}
-componentWillMount(){
-    this.handleAddContact();
-}
+ componentDidMount(){
+     this.getPeople();
+ }
+// componentWillMount(){
+//     this.handleAddContact();
+// }
 
 getPeople = (people) =>{
     fetch("http://localhost:4000/")
@@ -34,28 +33,26 @@ getPeople = (people) =>{
     
 }
 handleAddContact = (postContact) =>{
-    fetch("http://localhost:4000/add_contact", {
-        method: 'POST', 
-        body: JSON.stringify(postContact)
+//    console.log(postContact)
+    fetch(`http://localhost:4000/add_contact/?fn=${postContact.firstName}&ln=${postContact.lastName}&tn=${postContact.phone}`)
+//    , {
+//        method: 'POST', 
+//        body: JSON.stringify(postContact)
         
-        })
-        .then(res => this.setState({postContact:postContact}))
+//        })
+//          .then(response => response.json())
+          .then(this.getPeople)
+//            this.setState({postContact:postContact}))
         .catch(error => console.error('Error:', error))
-        .then(response =>  console.log('Success:', response))
 }
 
-
-
-
-    
     render(){
         
-
         return(
             <div>
             <Header/>
             <ListOfContacts people = {this.state.people} getPeople ={this.getPeople}/>
-            <FormContainer handleAddContact = {this.handleAddContact}/>
+            <FormContainer handleAddContact = {this.handleAddContact} />
             </div>
         )
     }
